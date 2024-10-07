1646a245f4fde2bd7141944a4c64559b2a329d0c
unknown <snehaldalavi20@gmail.com>
2024-10-07 14:53:54 +0530

    onbording_Pages1

---
+++ new.js
@@ -0,0 +1,24 @@
+// function goToNextPage() {
+//     const selectedData = ['Core Java', 'Javascript', 'jQuery', '.net', 'React', 'c#', 'Python', 'PHP', 'SQL'];
+//     localStorage.setItem('selectedData', JSON.stringify(selectedData));
+//     window.location.href = 'index2.html';
+//   }
+
+// function onPageLoad() {
+//     const selectedData = JSON.parse(localStorage.getItem('selectedData'));
+//     if (selectedData) {
+//         const reversedData = selectedData.reverse();
+//         document.getElementById('data').innerText = reversedData.join(', ');
+//     }
+//   }
+
+// //   Hamburger
+//   document.getElementById("toggleButton").onclick= function(){
+//     var menu = document.getElementById("menu");
+//     if (menu.style.display==="none") {
+//         menu.style.display=== "block";
+//     }
+//     else{
+//         menu.style.display==="none"
+//     }
+// } 
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors : {
+        black : {
+          '50' : '#3d3c3c',
+          '100' : '#000000'
+        },
+      },
+      width: {
+        '3.5' : '0.875rem',
+        '70' : '20.313rem'
+      },
+      height : {
+         '3.5' : '0.875rem'
+      },
+      borderWidth : {
+        '1' : '0.063rem',
+      },
+      gap : {
+        '33' : '8.75rem'
+      },
+      margin : {
+        '15' : '3.75rem'
+      },
+    },
+  },
+  plugins: [],
+}
+    
+ 
+    
